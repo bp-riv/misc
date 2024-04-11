4aabd1ac0e0ab3db452311671919511877822d3c
bp-riv <165778361+bp-riv@users.noreply.github.com>
2024-04-11 11:51:56 +0200

    mew version based on sorting.

--- var.py
+++ var.py
@@ -4,25 +4,25 @@ import matplotlib.pyplot as plt
 
 
 def historic_cdf (samples, bound):
-    '''
-    Returns the amount of samples with values
-    inferior than the bound, in form of probability [0,1]
-    '''
-    samples = samples.reshape(-1,)
-    satisfy = torch.sum(samples < bound)
-    return satisfy / len(samples)
+	'''
+	Returns the amount of samples with values
+	inferior than the bound, in form of probability [0,1]
+	'''
+	samples = samples.reshape(-1,)
+	satisfy = torch.sum(samples < bound)
+	return satisfy / len(samples)
 ####
 
 
 def grid_quantile(samples, confidence, n_mesh):
-    '''
-    
-    '''
+	'''
+	
+	'''
 	samples = samples.reshape(-1,)
 	max_sample = torch.max(samples)
 	min_sample = torch.min(samples)
 	h = (max_sample - min_sample) / n_mesh
-    upper_q = max_sample
+		upper_q = max_sample
 	lower_q = upper_q - h
 	while (condition == False and lower_q > min_sample):
 		p_upper = historic_cdf(samples, upper_q)
